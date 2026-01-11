77958f6d40e44e2be790eaf5d68aebc4af573e06
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-01-11 23:32:42 +0600

    added function for debounce

--- js/practice.js
+++ js/practice.js
@@ -68,33 +68,15 @@ function debounceAdvanced(func, delay, immediate = false) {
   };
 }
 
-function deepClone(obj) {
-  // Handle primitives, null, and undefined
-  if (obj === null || typeof obj !== 'object') {
-    return obj;
-  }
+function debounce(func, delay) {
+  let timeoutId;
   
-  // Handle Date
-  if (obj instanceof Date) {
-    return new Date(obj.getTime());
+  return function(...args) {
+    // Clear the previous timeout
+    clearTimeout(timeoutId);
+    
+    // Set a new timeout
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
   }
-  
-  // Handle Array
-  if (Array.isArray(obj)) {
-    return obj.map(item => deepClone(item));
-  }
-  
-  // Handle Object
-  if (typeof obj === 'object') {
-    const clonedObj = {};
-    for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        clonedObj[key] = deepClone(obj[key]);
-      }
-    }
-    return clonedObj;
-  }
-  
-  // Fallback (should not reach here for valid inputs)
-  return obj;
-}
