5a958e2c5ba54a9410c1661c2c77ce10b90fb3c0
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-01-14 00:17:31 +0600

    added functionality for debounce

--- js/practice.js
+++ js/practice.js
@@ -165,3 +165,17 @@ function deepEqual(obj1, obj2, visited = new WeakMap()) {
   
   return true;
 }
+
+function debounce(func, delay) {
+  let timeoutId;
+  
+  return function(...args) {
+    // Clear the previous timeout
+    clearTimeout(timeoutId);
+    
+    // Set a new timeout
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
