ac90c6d4a4b023e0d2842e77ac4af8f4647a0d2d
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-01-19 20:20:55 +0600

    added functionality for throttle

--- js/practice.js
+++ js/practice.js
@@ -228,3 +228,15 @@ function deepEqual(obj1, obj2) {
   
   return true;
 }
+
+function throttle(func, limit) {
+  let inThrottle;
+  
+  return function(...args) {
+    if (!inThrottle) {
+      func.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => inThrottle = false, limit);
+    }
+  };
+}
