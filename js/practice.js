ad6386a6eaf233b3356e1e7589f1c15b23f00d9a
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-03-07 23:07:53 +0600

    added functionality to get the largest value

--- js/practice.js
+++ js/practice.js
@@ -404,3 +404,16 @@ function throttle(func, limit) {
     }
   };
 }
+
+
+function findLargestNumber(arr) {
+  let largest = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+
+  return largest;
+}
