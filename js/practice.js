9727a3c6ba9c62757be5fde179657c8370bc0642
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-03-09 19:28:05 +0600

    added debounced functionality

--- js/practice.js
+++ js/practice.js
@@ -419,3 +419,17 @@ function isPalindrome(str) {
 
   return true;
 }
+
+function debounce(func, delay) {
+  let timeoutId;
+  
+  return function(...args) {
+    // Clear the previous timeout
+    clearTimeout(timeoutId);
+    
+    // Set a new timeout
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
