49d53149be612314381077a085992946707362e0
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-05-18 23:27:07 +0600

    feat(utils): add array equality helper function

--- js/practice.js
+++ js/practice.js
@@ -456,3 +456,21 @@ function throttle(func, limit) {
     }
   };
 }
+
+function AreArraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    
+    // Note: This doesn't handle duplicates correctly!
+    const set1 = new Set(arr1);
+    const set2 = new Set(arr2);
+    
+    if (set1.size !== set2.size) return false;
+    
+    for (let item of set1) {
+        if (!set2.has(item)) return false;
+    }
+    
+    return true;
+}
+
+// console.log(AreArraysEqual([1, 2, 3], [3, 2, 1]));  
