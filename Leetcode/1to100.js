09a573a62c0a42ac6b50e4e6eb372b572e38cad8
Tanvir islam akash <tanvirislamakash2002@gmail.com>
2026-07-04 22:50:41 +0600

    Created a function that can take an array of number and a terger value. And return the sum of two numbers which is equal to the target value.

---
+++ Leetcode/1to100.js
@@ -0,0 +1,11 @@
+const twoSum = function(nums, target) {
+    // Option 2: Hash map (O(n))
+    const seen = {};
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (seen[complement] !== undefined) {
+            return [seen[complement], i];
+        }
+        seen[nums[i]] = i;
+    }
+};
